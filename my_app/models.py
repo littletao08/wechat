ed7286f70c2f6fe3b30e5d83523b998277665072
meo <305233442@qq.com>
2017-04-16 13:35:31 +0800

    使用flask_login插件, 并改动数据模型

--- my_app/models.py
+++ my_app/models.py
@@ -3,18 +3,22 @@
 """
 数据库模型
 """
-
+from flask_login import UserMixin
 from werkzeug import generate_password_hash, check_password_hash
 
-from my_app import db
+from my_app import db, login_manager
 
 
-class Account(db.Model):
+class Account(UserMixin, db.Model):
     """微信公众平台用户类"""
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(255), unique=True)
     email = db.Column(db.String(255), unique=True)
     password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
 
     @password.setter
     def password(self, password):
@@ -22,7 +26,12 @@ class Account(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
+
+@login_manager.user_loader
+def load_user(user_id):
+      return User.query.get(int(user_id))
+
 
 class Token(db.Model):
     """存放AccessToken的类"""
@@ -30,6 +39,7 @@ class Token(db.Model):
     app_id = db.Column(db.String(50), unique=True)
     app_secret = db.Column(db.String(256))
     wechat_id = db.Column(db.String(256))
+    token = db.Column(db.String(256))
     access_token = db.Column(db.String(256))
     expired_time = db.Column(db.Integer)
     account_id = db.Column(db.String(50), db.ForeignKey('account.id'))
