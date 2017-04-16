c5e2122905337cda30d6601db7b3213438b1092f
meo <305233442@qq.com>
2017-04-17 00:44:30 +0800

    修改微信接入视图, 从数据库中获取token信息

--- my_app/main/views.py
+++ my_app/main/views.py
@@ -5,6 +5,7 @@ from flask.views import MethodView
 
 from my_app import db
 from tools import check_signature
+from my_app.models import Token
 
 
 wechat = Blueprint('wechat', __name__)
@@ -12,19 +13,27 @@ wechat = Blueprint('wechat', __name__)
 class MainView(MethodView):
     """用于接入微信的视图类"""
     def get(self):
+        app_id = request.args.get('app_id')
         signature = request.args.get('signature')
         timestamp = request.args.get('timestamp')
         nonce = request.args.get('nonce')
         echostr = request.args.get('echostr')
-        token = current_app.config['WECHAT_TOKEN']
 
-        try:
-            if check_signature(signature, timestamp, nonce, token):
+        t = Token.query.filter_by(app_id=app_id).first()
+
+        if t is not None:
+            token = t.token
+
+            try:
+                if check_signature(signature, timestamp, nonce, token):
+                    return echostr
+                else:
+                    print u'错误'
+                    return ""
+            except Exception as e:
                 return echostr
-            else:
-                return ""
-        except Exception as e:
-            return echostr
+
+        return ""
 
     def post(self):
        pass
