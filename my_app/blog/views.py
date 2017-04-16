2f529d921246acf4fd24d198883d207dc129e403
meo <305233442@qq.com>
2017-04-16 23:18:52 +0800

    增加注册视图

--- my_app/blog/views.py
+++ my_app/blog/views.py
@@ -3,8 +3,9 @@
 from flask import request, Blueprint, render_template, redirect, flash, url_for
 from flask_login import current_user, login_user
 
-from my_app.blog.forms import LoginForm
+from my_app.blog.forms import LoginForm, RegisterForm
 from my_app.models import Account
+from my_app import db
 
 blog = Blueprint('blog', __name__)
 
@@ -13,9 +14,45 @@ blog = Blueprint('blog', __name__)
 def index():
     return render_template('blog/index.html')
 
-@blog.route('/regist', methods=['GET', 'POST'])
-def regist():
-    pass
+@blog.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    注册视图
+
+    """
+    if current_user.is_authenticated:
+        flash(u'您已登录!', 'info')
+        return redirect(url_for('blog.index'))
+
+    form = RegisterForm(request.form)
+
+    if form.validate_on_submit():
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        existing_username = Account.query.filter_by(name=name).first()
+
+        if existing_username:
+            flash(
+                u'用户名或电子邮箱已存在, 请重新输入!',
+                'warning'
+                )
+            return render_template('blog/register.html', form=form)
+
+        account = Account()
+        account.name = name
+        account.email = email
+        account.password = password
+        db.session.add(account)
+        db.session.commit()
+        flash(u'您已注册, 请登录', 'success')
+        return redirect(url_for('blog.index'))
+
+    if form.errors:
+        flash(form.errors, 'danger')
+
+    return render_template('blog/register.html', form=form)
 
 @blog.route('/login', methods=['GET', 'POST'])
 def login():
