688a409471b7aff1b8be54afddb6b33144a63512
meo <305233442@qq.com>
2017-08-18 20:04:19 +0800

    更改消息模型外键问题, 更改字符串字段长度

--- my_app/models.py
+++ my_app/models.py
@@ -36,11 +36,11 @@ def load_user(user_id):
 class Token(db.Model):
     """存放AccessToken的类"""
     id = db.Column(db.Integer, primary_key=True)
-    app_id = db.Column(db.String(50), unique=True)
-    app_secret = db.Column(db.String(256))
-    wechat_id = db.Column(db.String(256))
-    token = db.Column(db.String(256))
-    access_token = db.Column(db.String(256))
+    app_id = db.Column(db.String(255), unique=True)
+    app_secret = db.Column(db.String(255))
+    wechat_id = db.Column(db.String(255))
+    token = db.Column(db.String(255))
+    access_token = db.Column(db.String(255))
     expired_time = db.Column(db.Integer)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     account = db.relationship('Account',
@@ -54,25 +54,25 @@ tags = db.Table('tags',
 class Tag(db.Model):
     """用户标签类"""
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
+    name = db.Column(db.String(255))
     count = db.Column(db.Integer)
 
 
 class User(db.Model):
     """用户类"""
     id = db.Column(db.Integer, primary_key=True)
-    app_id = db.Column(db.String(256))
-    openid = db.Column(db.String(256))
-    nickname = db.Column(db.String(256))
-    sex = db.Column(db.String(256))
-    language = db.Column(db.String(256))
-    city = db.Column(db.String(256))
-    province = db.Column(db.String(256))
-    country = db.Column(db.String(256))
-    headimageurl = db.Column(db.String(256))
+    app_id = db.Column(db.String(255))
+    openid = db.Column(db.String(255))
+    nickname = db.Column(db.String(255))
+    sex = db.Column(db.String(255))
+    language = db.Column(db.String(255))
+    city = db.Column(db.String(255))
+    province = db.Column(db.String(255))
+    country = db.Column(db.String(255))
+    headimageurl = db.Column(db.String(255))
     subscribe_time = db.Column(db.Integer)
-    unionid = db.Column(db.String(256))
-    remark = db.Column(db.String(256))
+    unionid = db.Column(db.String(255))
+    remark = db.Column(db.String(255))
     tags = db.relationship('Tag', secondary=tags,
                            backref=db.backref('users', lazy='dynamic'))
 
@@ -80,12 +80,12 @@ class User(db.Model):
 class Msg(db.Model):
     """消息记录存储模型"""
     id = db.Column(db.Integer, primary_key=True)
-    from_username = db.Column(db.String(256))
-    to_username = db.Column(db.String(256))
-    msg_type = db.Column(db.String(256))
+    from_username = db.Column(db.String(255))
+    to_username = db.Column(db.String(255))
+    msg_type = db.Column(db.String(255))
     create_time = db.Column(db.Integer)
-    content = db.Column(db.String(256))
-    media_id = db.Column(db.String(256), db.ForeignKey('media.media_id'))
+    content = db.Column(db.String(255))
+    media_id = db.Column(db.Integer, db.ForeignKey('media.id'))
     media = db.relationship('Media',
                             backref=db.backref('msgs', lazy='dynamic'))
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
@@ -99,19 +99,19 @@ class Msg(db.Model):
 class Media(db.Model):
     """素材对象的数据模型"""
     id = db.Column(db.Integer, primary_key=True)
-    media_id = db.Column(db.String(256))
-    thumb_media_id = db.Column(db.String(256))
-    media_type = db.Column(db.String(256))
-    voice_format = db.Column(db.String(256))
-    voice_recognition = db.Column(db.String(256))
-    title = db.Column(db.String(256))
-    description = db.Column(db.String(256))
-    music_url = db.Column(db.String(256))
-    hq_music_url = db.Column(db.String(256))
-    locale_url = db.Column(db.String(256))
-    tecent_url = db.Column(db.String(256))
+    media_id = db.Column(db.String(255))
+    thumb_media_id = db.Column(db.String(255))
+    media_type = db.Column(db.String(255))
+    voice_format = db.Column(db.String(255))
+    voice_recognition = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    music_url = db.Column(db.String(255))
+    hq_music_url = db.Column(db.String(255))
+    locale_url = db.Column(db.String(255))
+    tecent_url = db.Column(db.String(255))
     expired_time = db.Column(db.Integer)
-    app_id = db.Column(db.String(256), db.ForeignKey('token.app_id'))
+    app_id = db.Column(db.String(255), db.ForeignKey('token.app_id'))
     app = db.relationship('Token',
                           backref=db.backref('medias', lazy='dynamic'))
 
@@ -119,9 +119,9 @@ class Media(db.Model):
 class Event(db.Model):
     """事件类型的东西"""
     id = db.Column(db.Integer, primary_key=True)
-    event_type = db.Column(db.String(256))
-    event_key = db.Column(db.String(256))
-    ticket = db.Column(db.String(256))
+    event_type = db.Column(db.String(255))
+    event_key = db.Column(db.String(255))
+    ticket = db.Column(db.String(255))
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     precision = db.Column(db.Float)
@@ -133,4 +133,4 @@ class Location(db.Model):
     location_x = db.Column(db.Float)
     location_y = db.Column(db.Float)
     scale = db.Column(db.Float)
-    label = db.Column(db.String(256))
+    label = db.Column(db.String(255))
