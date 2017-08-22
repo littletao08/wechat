4b299380b873610e02d56b9481f1abab827fd264
meo <305233442@qq.com>
2017-08-23 00:05:26 +0800

    测试多种类型消息回复

--- my_app/main/views.py
+++ my_app/main/views.py
@@ -1,24 +1,34 @@
 # coding: utf-8
 
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request
 from flask.views import MethodView
 
-from my_app import db
 from tools import check_signature
 from my_app.models import Token
+from my_app import db
 import receive
 import reply
 
-
 wechat = Blueprint('wechat', __name__)
 
+
 class MainView(MethodView):
+    """微信业务逻辑视图
+
+    微信接入, 负责微信业务接入及消息处理的视图类
+
+    Attributes:
+        wechat: wechat蓝图
     """
-    用于接入微信的视图类
-    接入token是在绑定时候填写的
-    接入地址是http://xxx.xx/wechat/?app_id=app_id
-    """
+
     def get(self):
+        """微信接入
+
+        get请求处理, 验证消息是否为微信服务器发来, 接入微信公众号
+
+        Returns:
+            校验成功返回get参数echostr
+        """
         app_id = request.args.get('app_id')
         signature = request.args.get('signature')
         timestamp = request.args.get('timestamp')
@@ -29,43 +39,62 @@ class MainView(MethodView):
 
         if t is not None:
             token = t.token
+            if check_signature(signature, timestamp, nonce, token):
+                return echostr
 
-            try:
-                if check_signature(signature, timestamp, nonce, token):
-                    return echostr
-                else:
-                    return u"校验不通过"
-            except Exception as e:
-                return str(e), u'校验过程出错'
-
-        return u"数据库比对失败"
+        return u"校验失败"
 
     def post(self):
-        """
-        简单自动回复, 识别消息类型
+        """处理微信服务器推送过来的消息及事件
+
+        处理微信服务器POST推送过来的各种消息及事件
+
+        Returns:
+            返回回复消息字符串, 微信公众平台文档中规定的精简版xml字符串消息
         """
         data = request.data
+        msg = receive.parse_xml(data)
         try:
-            msg = receive.parse_xml(data)
-            reply_msg = reply.TextMsg()
-            reply_msg.FromUserName = msg.ToUserName
-            reply_msg.ToUserName = msg.FromUserName
-            if isinstance(msg, receive.EventMsg):
-                if msg.Event == 'subscribe':
-                    reply_msg.Content = u'欢迎订阅'
-                else:
-                    reply_msg.Content = u'这是一条' + msg.Event + u'事件'
+            m = msg.save()
+            db.session.add(m)
+            db.session.commit()
+        except Exception as e:
+            print str(e)
+
+        reply_msg = reply.TextMsg()
+        if isinstance(msg, receive.EventMsg):
+            if msg.Event == 'subscribe':
+                reply_msg.Content = u'欢迎订阅'
+            else:
+                reply_msg.Content = u'这是一条' + msg.Event + u'事件'
+        else:
+            if msg.MsgType == 'image':
+                reply_msg = reply.ImageMsg()
+                image = reply.Image(msg.MediaId)
+                reply_msg.Image = image
+            elif msg.MsgType == 'voice':
+                reply_msg = reply.VoiceMsg()
+                voice = reply.Voice(msg.MediaId)
+                reply_msg.Voice = voice
+            elif msg.MsgType == 'video':
+                reply_msg = reply.VideoMsg()
+                video = reply.Video()
+                video.MediaId = msg.MediaId
+                video.Description = 'just test'
+                video.Title = 'Test'
+                reply_msg.Video = video
             else:
                 reply_msg.Content = u'这是一条' + msg.MsgType + u'消息'
+        reply_msg.FromUserName = msg.ToUserName
+        reply_msg.ToUserName = msg.FromUserName
+
+        try:
             return reply_msg.send()
         except Exception as e:
-            return 'success'
+            print str(e)
         finally:
-            try:
-                msg.save()
-                reply_msg.save()
-            except Exception as e:
-                db.session.rollback()
+            reply_msg.save()
+
 
 main_view = MainView.as_view('main_view')
 wechat.add_url_rule('/', view_func=main_view, methods=['GET', 'POST'])
