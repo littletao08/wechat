840e9d01c9e0e2922564b73d20fa449bcf43a760
meo <305233442@qq.com>
2017-08-24 20:48:14 +0800

    修改主视图

--- my_app/main/views.py
+++ my_app/main/views.py
@@ -76,7 +76,7 @@ class MainView(MethodView):
                 reply_msg = reply.VoiceMsg()
                 voice = reply.Voice(msg.MediaId)
                 reply_msg.Voice = voice
-            elif msg.MsgType == 'video' or msg.MsgType == 'shortvideo':
+            elif msg.MsgType == 'video':
                 reply_msg = reply.VideoMsg()
                 video = reply.Video()
                 video.MediaId = msg.MediaId
